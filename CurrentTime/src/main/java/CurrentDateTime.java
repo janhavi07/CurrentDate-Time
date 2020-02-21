459a966084d8b6eba41f2ce320ecb19df7a99952
Janhavi Parte <janhavi.parte@Janhavis-MacBook-Pro.local>
2020-02-21 13:12:07 +0530

    Added Code: To print the current year

--- CurrentTime/src/main/java/CurrentDateTime.java
+++ CurrentTime/src/main/java/CurrentDateTime.java
@@ -3,12 +3,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 public class CurrentDateTime {
     public static void main(String[] args) throws IOException {
         LocalDateTime current = LocalDateTime.now();
+        Calendar now = Calendar.getInstance();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
         String formatted = current.format(formatter);
+        System.out.println("Current Year is : " + now.get(Calendar.YEAR));
         System.out.println("Hey Janhavi Parte , this is the Current Date and Time : " + formatted);
         BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/janhavi.parte/Desktop/Projects/CurrentTime/src/main/resources/CurrentDateTimeFile.txt"));
         writer.write(formatted);
