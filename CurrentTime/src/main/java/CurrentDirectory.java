84cec039cb5232055f1e61da784865c4dbed0765
Janhavi Parte <janhavi.parte@Janhavis-MacBook-Pro.local>
2020-02-24 16:23:15 +0530

    Added a code to find out new directory

---
+++ CurrentTime/src/main/java/CurrentDirectory.java
@@ -0,0 +1,9 @@
+public class CurrentDirectory {
+    public static void main(String[] args) {
+
+        String path = System.getProperty("user.dir");
+
+        System.out.println("Working Directory = " + path);
+
+    }
+}
