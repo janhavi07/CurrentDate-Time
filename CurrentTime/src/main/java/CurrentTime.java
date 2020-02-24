5f32c644171c2f6abcb8f92247520816eab05611
Janhavi Parte <janhavi.parte@Janhavis-MacBook-Pro.local>
2020-02-24 16:41:48 +0530

    Added a new program of hellow

--- CurrentTime/src/main/java/CurrentTime.java
+++ CurrentTime/src/main/java/CurrentTime.java
@@ -8,10 +8,8 @@ import java.util.Calendar;
 public class CurrentTime {
     public static void main(String[] args) throws IOException {
         LocalDateTime current = LocalDateTime.now();
-        Calendar now = Calendar.getInstance();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
         String formatted = current.format(formatter);
-      System.out.println("Current Year is : " + now.get(Calendar.YEAR));
         System.out.println("Hey Janhavi Parte , this is the Current Date and Time : " + formatted);
         BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/janhavi.parte/Desktop/Projects/CurrentTime/src/main/resources/CurrentDateTimeFile.txt"));
         writer.write(formatted);
